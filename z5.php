02dd97a548283320fd0dcc64d856572d02249af4
MEJXIK <90198615+MEJXIK@users.noreply.github.com>
2021-09-20 17:14:17 +0200

    Add files via upload

---
+++ z5.php
@@ -0,0 +1,16 @@
+<?php
+$x = 2;
+//deklarujemy zmienna x i przypisujemy jej wartosc 2
+$y = 3;
+//deklarujemy zmienna y i przypisujemy jej wartosc 3
+$z = 5;
+//deklarujemy zmienna z i przypisujemy jej wartosc 5
+$x++;
+//inkrementujemy wartosc zmiennej x
+$y--;
+//dekrementujemy wartosc zmiennej y
+$z += $x;
+//przypisujemy zmiennej z wartosc sumy zmiennej z i x 
+--$z
+//dekrementujemy wartosc zmiennej z
+?>
